060b060dbd1320af956c488b77862c203a24aa92
tawatson02 <ta_watson@live.com>
2024-05-29 20:00:52 -0500

    did work on js and hbs

---
+++ public/js/logout.js
@@ -0,0 +1,17 @@
+//Needs button created and code edited
+
+ //const logout = async () => {
+//     const response = await fetch('', {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+  
+//     if (response.ok) {
+//       document.location.replace('/');
+//     } else {
+//       alert('Failed to log out.');
+//     }
+//   };
+  
+//   document.querySelector('#logout').addEventListener('click', logout);
+  
