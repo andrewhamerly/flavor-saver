1191a3cee03380b8da9146f443319368ba5d36ab
Andrew Hamerly <73661602+andrewhamerly@users.noreply.github.com>
2024-06-03 18:16:44 -0400

    Fix server.js file to use hbs file extension for app.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,7 +9,6 @@ router.get('/', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['name'],
         },
       ],
     });
@@ -23,6 +22,7 @@ router.get('/', async (req, res) => {
       logged_in: req.session.logged_in 
     });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
--- server.js
+++ server.js
@@ -31,9 +31,9 @@ const sess = {
 
 app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
