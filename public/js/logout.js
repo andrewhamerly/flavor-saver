7b89231d03c0ffd8178701fadcdc4c9875910cfb
chrislose23 <chris@Christophers-MacBook-Air.local>
2024-06-04 23:47:53 -0400

    fixed logout function

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,17 +1,15 @@
-const logoutHandler = async (event) => {
-  event.preventDefault();
-
-  const response = await fetch('/api/users/logout', {
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-  });
-
-  if (response.ok) {
-      document.location.replace('/login');
-  } else {
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
       alert('Failed to log out.');
-  }
-};
-
-document.querySelector('#logout').addEventListener('click', logoutHandler);
+    }
+  };
+  
+  document.querySelector('#logout').addEventListener('click', logout);
   
